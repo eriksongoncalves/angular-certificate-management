270c744c9e4a270176c111dffbd4869ce728e7b8
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-05-27 20:23:41 -0300

    feat: primary button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
+
 import { NavbarComponent } from './_components/navbar/navbar.component'
+import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
