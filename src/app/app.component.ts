1364e897807710873cc779ee815c990c5f562fa4
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-05-27 20:25:41 -0300

    feat: secondary button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 
 import { NavbarComponent } from './_components/navbar/navbar.component'
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component'
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, PrimaryButtonComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
