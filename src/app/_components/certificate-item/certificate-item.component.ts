9e035cc8d68caa09607f90b26fd1c1fddf42ffe7
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-06-25 22:57:18 -0300

    feat: apply router link

--- src/app/_components/certificate-item/certificate-item.component.ts
+++ src/app/_components/certificate-item/certificate-item.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component'
 
 @Component({
   selector: 'app-certificate-item',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificate-item.component.html',
   styleUrl: './certificate-item.component.scss'
 })
--- src/app/pages/certificate/certificate.component.ts
+++ src/app/pages/certificate/certificate.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component'
 
 @Component({
   selector: 'app-certificate',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificate.component.html',
   styleUrl: './certificate.component.scss'
 })
